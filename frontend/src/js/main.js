81431e1c4f10816903547f44ac718bb594887253
James Groves <triskept@gmail.com>
2019-11-18 16:56:34 -0500

    try2

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,29 @@
+import apiActions from './apiActions/apiActions'
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/list/main.css';
+
+
 export default () => {
-    alert('working')
+    pageBuild()
+}
+
+function pageBuild(){
+    calendar()
+}
+
+function calendar(){
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+      
+        let calendar = new Calendar(calendarEl, {
+            plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ]
+          });      
+        calendar.render();
+      });
 }
