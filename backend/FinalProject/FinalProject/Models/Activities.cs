782d6d620f901ae692060d0244b162300f033bd1
James Groves <triskept@gmail.com>
2019-11-18 11:47:14 -0500

    made schedule model

---
+++ backend/FinalProject/FinalProject/Models/Activities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Activities
+    {
+        public int Id { get; set; }
+        public string AgeRange { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public int Score { get; set; }
+
+        public virtual int ScheduleId { get; set; }
+        public virtual Schedule Schedule { get; set; }
+
+        public Activities()
+        {
+
+        }
+    }
+}
---
+++ backend/FinalProject/FinalProject/Models/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+
+        public virtual ICollection<Activities> Activities { get; set; }
+
+        public Schedule()
+        {
+
+        }
+    }
+}
