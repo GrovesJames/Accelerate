4f27b4eca45161bc6a5966bad4eceeacdc7396b9
Steven <steventruong1995@yahoo.com>
2019-11-18 14:21:23 -0500

    schedule repository

---
+++ backend/FinalProject/FinalProject/Repositories/ScheduleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Models;
+
+namespace FinalProject.Repositories
+{
+    public class ScheduleRepository : Repository<Schedule>, IRepository<Schedule>
+    {
+        public ScheduleRepository(Context context) : base(context)
+        {
+
+        }
+    }
+}
