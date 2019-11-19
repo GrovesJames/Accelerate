ed8ceff7c1df366536f24abd548e69ffd0b9caf9
dzetzer <dillon.zetzer@sbcglobal.net>
2019-11-19 04:22:20 -1100

    fixed controllers

--- backend/FinalProject/FinalProject/Controllers/ActivitiesController.cs
+++ backend/FinalProject/FinalProject/Controllers/ActivitiesController.cs
@@ -8,51 +8,55 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
 {
-    public class ActivitiesController
+    [Route("api/activities")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
     {
-        private IRepository<Activities> activitiesRepo;
-        public ActivitiesController(IRepository<Activities> activitiesRepo)
+
+        private IRepository<Activities> ActivitiesRepo;
+
+        public ActivitiesController(IRepository<Activities> ActivitiesRepo)
         {
-            this.activitiesRepo = activitiesRepo;
+            this.ActivitiesRepo = ActivitiesRepo;
         }
 
-        // GET api/activities
+        // GET api/Activitiess
         [HttpGet]
         public IEnumerable<Activities> Get()
         {
-            return activitiesRepo.GetAll();
+            return ActivitiesRepo.GetAll();
         }
 
-        // GET api/activities/5
+        // GET api/Activitiess/5
         [HttpGet("{id}")]
         public Activities Get(int id)
         {
-            return activitiesRepo.GetById(id);
+            return ActivitiesRepo.GetById(id);
         }
 
-        // POST api/activities
+        // POST api/Activitiess
         [HttpPost]
-        public IEnumerable<Activities> Post([FromBody] Activities activities)
+        public IEnumerable<Activities> Post([FromBody] Activities Activities)
         {
-            activitiesRepo.Create(activities);
-            return activitiesRepo.GetAll();
+            ActivitiesRepo.Create(Activities);
+            return ActivitiesRepo.GetAll();
         }
 
-        // PUT api/activities/5
+        // PUT api/Activitiess/5
         [HttpPut("{id}")]
-        public IEnumerable<Activities> Put(int id, [FromBody] Activities activities)
+        public IEnumerable<Activities> Put([FromBody] Activities Activities)
         {
-            activitiesRepo.Update(activities);
-            return activitiesRepo.GetAll();
+            ActivitiesRepo.Update(Activities);
+            return ActivitiesRepo.GetAll();
         }
 
-        // DELETE api/activities/5
+        // DELETE api/Activitiess/5
         [HttpDelete("{id}")]
         public IEnumerable<Activities> Delete(int id)
         {
-            var activities = activitiesRepo.GetById(id);
-            activitiesRepo.Delete(activities);
-            return activitiesRepo.GetAll();
+            var Activities = ActivitiesRepo.GetById(id);
+            ActivitiesRepo.Delete(Activities);
+            return ActivitiesRepo.GetAll();
         }
     }
 }
--- backend/FinalProject/FinalProject/Repositories/ScheduleRepository.cs
+++ backend/FinalProject/FinalProject/Repositories/ScheduleRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject.Data;
 using FinalProject.Models;
 
 namespace FinalProject.Repositories
