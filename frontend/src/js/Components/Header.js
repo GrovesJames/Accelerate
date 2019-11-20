4502d3f950e943807b7eeaefc5cfc752b782d52b
James Groves <triskept@gmail.com>
2019-11-20 10:50:44 -0500

    nav colors

--- frontend/src/js/Components/Header.js
+++ frontend/src/js/Components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
     return `
     <div id="header">
-    <img src='./images/AccelerateLOGO.png'></img>
     <h1>Accelerate</h1>
+    <img src='./images/AccelerateLOGO.png'></img>
     </div>
     `
 }
