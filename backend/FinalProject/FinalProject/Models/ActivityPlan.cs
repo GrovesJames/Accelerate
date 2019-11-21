df207a2bbe4b3e028d87ac3c2b3b00d8e3446ee1
dzetzer <dillon.zetzer@sbcglobal.net>
2019-11-21 07:23:44 -1100

    added activity plan mode, controller, and repo

---
+++ backend/FinalProject/FinalProject/Models/ActivityPlan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class ActivityPlan
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+        public int Duration { get; set; }
+
+        public ActivityPlan( int id, string title, string description, int score, int duration)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Score = score;
+            Duration = duration;
+        }
+        public ActivityPlan()
+        {
+
+        }
+    }
+}
