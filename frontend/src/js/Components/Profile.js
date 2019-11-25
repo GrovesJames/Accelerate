5b5151d91c302078bbf274f15f3335d6572246ff
dzetzer <dillon.zetzer@sbcglobal.net>
2019-11-25 08:03:57 -1100

    skill buttons

--- frontend/src/js/Components/Profile.js
+++ frontend/src/js/Components/Profile.js
@@ -10,9 +10,9 @@ export default function Profile(profile) {
             <h1>${profile.userName}</h1>
         </div>
         <div id="profile-skills">
-            <button value="1" class="button-profile-skill">Skill 1</button>
-            <button value="2" class="button-profile-skill">Skill 2</button>
-            <button value="3" class="button-profile-skill">Skill 3</button>
+            <button value="1" class="button-profile-skill">Speech</button>
+            <button value="2" class="button-profile-skill">Cognitive Functionality</button>
+            <button value="3" class="button-profile-skill">Motor Skills</button>
         </div>
         <div id="profile-calendar"></div>
     `
