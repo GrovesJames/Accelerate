bb47432150e5842355ac108b2b3178ddde20f1a0
James Groves <triskept@gmail.com>
2019-11-25 16:19:05 -0500

    added individual skill page functionality

--- frontend/src/js/Components/Profile.js
+++ frontend/src/js/Components/Profile.js
@@ -9,11 +9,5 @@ export default function Profile(profile) {
         <div id="profile-main">
             <h1>${profile.userName}</h1>
         </div>
-        <div id="profile-skills">
-            <button value="1" class="button-profile-skill">Speech</button>
-            <button value="2" class="button-profile-skill">Cognitive Functionality</button>
-            <button value="3" class="button-profile-skill">Motor Skills</button>
-        </div>
-        <div id="profile-calendar"></div>
     `
 }
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -66,7 +66,7 @@ function login(){
     app.innerHTML = Login();
 }
 function addSkillSelectButtons(){
-    const skillButtons = document.getElementsByClassName("button-profile-skill");
+    const skillButtons = document.getElementsByClassName("button-skill");
     for (var i = 0; i < skillButtons.length; i++) {
         skillButtons[i].addEventListener('click', function(){
             const skillId = event.target.value;
@@ -94,11 +94,12 @@ function skillsNAV() {
     const navSkills = document.querySelector('#skillsnav');
     navSkills.addEventListener('click', function() {
         skills()
+        addSkillSelectButtons()
         closeNAV()
         document.querySelector('html').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.705)), url("/images/teacher3.jpg")';
     });
     app.addEventListener('click', function(){
-        if(event.target.classList.contains("acitvityDetails")){
+        if(event.target.classList.contains("activityDetails")){
             const activityPlanID = event.target.parentElement.querySelector(".activities_id")
             .value;
             apiActions.getRequest(`https://localhost:44355/api/activityplans/${activityPlanID}`,
@@ -191,8 +192,6 @@ function profileNAV(){
     navProfile.addEventListener('click', function() {
         apiActions.getRequest("https://localhost:44355/api/profile/1", profile => {
             app.innerHTML = Profile(profile);
-            addSkillSelectButtons();
-            calendar(document.getElementById("profile-calendar"));
         });
         closeNAV()
     });
