2de5d40264449b7aa0d73d4e4a3bb6edd3ae76e5
dzetzer <dillon.zetzer@sbcglobal.net>
2019-11-25 10:39:08 -1100

    activityplan submits new activity and renders on calendar

--- frontend/src/js/Components/ActivityPlan.js
+++ frontend/src/js/Components/ActivityPlan.js
@@ -4,12 +4,12 @@ export default function ActivityPlan(activityPlan) {
     <span>${activityPlan.description}</span>
     <span>${activityPlan.duration}</span>
     <form>
-        <input class="activity-plan_id" type="hidden" value=${activityPlan.id}
-        <input class="activity-plan_title" type="hidden" value=${activityPlan.title}
-        <input class="activity-plan_description" type="hidden" value=${activityPlan.description}
-        <input class="activity-plan_score" type="hidden" value=${activityPlan.score}
-        <input class="activity-plan_duration" type="hidden" value=${activityPlan.duration}
-        <input class="add-activity_plan" type="datetime-local"/>  
+        <input class="activity-plan_id" type="hidden" value=${activityPlan.id}>
+        <input class="activity-plan_title" type="hidden" value=${activityPlan.title}>
+        <input class="activity-plan_description" type="hidden" value=${activityPlan.description}>
+        <input class="activity-plan_score" type="hidden" value=${activityPlan.score}>
+        <input class="activity-plan_duration" type="hidden" value=${activityPlan.duration}>
+        <input class="add-activity_plan" type="datetime-local">  
         <input class="activity-plan_submit" type="submit" value="Submit"> 
     </form>
     `
