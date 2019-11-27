831d7816db74559afaef76552857b2030c290c9e
khoalim <kthoalim@gmail.com>
2019-11-27 11:18:40 -0500

    Updated Profile Component

--- frontend/src/js/Components/Profile.js
+++ frontend/src/js/Components/Profile.js
@@ -1,7 +1,9 @@
 export default function Profile(profile) { 
     return `
         <div id="profile-main">
-            <h1>${profile.userName}</h1>
+            <h1>Username: ${profile.userName}</h1>
+            <h1>Child's Name: ${profile.childName}</h1>
+            <h1>Child's Age: ${profile.childAge}</h1>
         </div>
     `
 }
