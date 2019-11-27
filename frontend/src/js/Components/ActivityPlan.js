9a01657da38d1c1d69111ed41947eb524c4cf33e
dzetzer <dillon.zetzer@sbcglobal.net>
2019-11-27 07:17:49 -1100

    fixed activity title

--- frontend/src/js/Components/ActivityPlan.js
+++ frontend/src/js/Components/ActivityPlan.js
@@ -5,7 +5,7 @@ export default function ActivityPlan(activityPlan) {
     <span>${activityPlan.duration}</span>
     <form>
         <input class="activity-plan_id" type="hidden" value=${activityPlan.id}>
-        <input class="activity-plan_title" type="hidden" value=${activityPlan.title}>
+        <input class="activity-plan_title" type="hidden" value="${activityPlan.title}">
         <input class="activity-plan_description" type="hidden" value=${activityPlan.description}>
         <input class="activity-plan_score" type="hidden" value=${activityPlan.score}>
         <input class="activity-plan_duration" type="hidden" value=${activityPlan.duration}>
