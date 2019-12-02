c50ee59c46671a7e0c9c05c1b20b5cc8fa4bb5ee
khoalim <kthoalim@gmail.com>
2019-12-02 10:05:31 -0500

    Made Updates after Merge

--- frontend/src/js/Components/ActivityPlan.js
+++ frontend/src/js/Components/ActivityPlan.js
@@ -20,22 +20,9 @@ export default function ActivityPlan(activityPlan) {
             </section>          
                       
             
-            `;             
+            `;        
+  
 
     
-
-    <h2>${activityPlan.title}</h2>
-    <span>${activityPlan.description}</span>
-    <span>${activityPlan.duration}</span>
-    <form>
-        <input class="activity-plan_id" type="hidden" value=${activityPlan.id}>
-        <input class="activity-plan_title" type="hidden" value="${activityPlan.title}">
-        <input class="activity-plan_description" type="hidden" value=${activityPlan.description}>
-        <input class="activity-plan_score" type="hidden" value=${activityPlan.score}>
-        <input class="activity-plan_duration" type="hidden" value=${activityPlan.duration}>
-        <input class="add-activity_plan" type="datetime-local">  
-        <button class="activity-plan_submit" type="button">Submit</button> 
-    </form>
-    `
 
 }
