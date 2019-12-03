5dce791fed9431d5541178cf40ed982412d01b23
Justin Brown <j-brown123@live.com>
2019-12-03 14:18:51 -0500

    workedon time

--- frontend/src/js/Components/ActivityDisplay.js
+++ frontend/src/js/Components/ActivityDisplay.js
@@ -1,8 +1,15 @@
 export default function DisplayActivity(activity) {
+   let activityDateObject = new Date(activity.activityTime)
+
+    let time = activityDateObject.toLocaleString("en-US", {timeZone: "America/New_York"})
+
+
+
     return `
+
         <li>
         <h2>${activity.title}</h2>
-        <p>${activity.activityTime}</p>
+        <p>${time}</p>
         <button class="activityday-delete-btn" value=${activity.id}>Delete</button>
         </li>
     `
