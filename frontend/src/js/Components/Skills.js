7ab45c737d117266675f6ebc93a45d38bdaaf91e
James Groves <triskept@gmail.com>
2019-12-04 10:52:22 -0500

    style skill page

--- frontend/src/js/Components/Skills.js
+++ frontend/src/js/Components/Skills.js
@@ -7,30 +7,33 @@ export default function Skills() {
             <p>Each 'skill' is broken down by its particular set of milestones, from which parents or caregivers can be educated on what areas of improvement are necessary for their child. Activities are then suggested that assist in the development and improvement of these skills. Afterwards, you may create a personalized profile for your child, where you can develop a schedule based on activities chosen.</p>
         </div>
 
+        <h3>Speech</h3>
         <div id="skill1grid">
             <div id="skill1name">
                 <button class="button-skill" value="1"></button>
-                <h3>Speech</h3>
+                <h3>Milestones</h3>
             </div>
             <div id="skill1act" class="skill1btns">
                 <button class="button-activities" value="1"></button>
                 <h6>Activities</h6>
             </div>
         </div>
+        <h3>Cognitive Functionality</h3>
         <div id="skill2grid">
             <div id="skill2name">
                 <button class="button-skill" value="2"></button>
-                <h3>Cognitive Functionality</h3>
+                <h3>Milestones</h3>
             </div>
             <div id="skill2act" class="skill2btns">
                 <button class="button-activities" value="2"></button>
                 <h6>Activities</h6>
             </div>
         </div>
+        <h3>Fine/Gross Motor Skills</h3>
         <div id="skill3grid">
             <div id="skill3name">
                 <button class="button-skill" value="3"></button>
-                <h3>Fine/Gross Motor Skills</h3>
+                <h3>Milestones</h3>
             </div>
             <div id="skill3act" class="skill3btns">
                 <button class="button-activities" value="3"></button>
